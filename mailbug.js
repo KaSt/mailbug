8053ee54cbc93ad14c242d0452499ce28676d073
Ka <k@ka.st>
2018-04-24 22:16:18 +0200

    Changed JQuery url to https

--- mailbug.js
+++ mailbug.js
@@ -1,7 +1,7 @@
 var script = document.createElement('script'); 
 document.head.appendChild(script);    
 script.type = 'text/javascript';
-script.src = "//ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js";
+script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js";
 script.onload = function(){
     $.get( "https://us-central1-kahoneypot.cloudfunctions.net/getIp", function( data ) {
     alert( data );
